789ffb2922d3dfd9c805cfb7b599ff1b66b49c4d
Gurupirasath <gurujivedhu007@gmail.com>
2024-10-07 20:51:47 +0530

    Created Parameter with Return.java

---
+++ Parameter with Return.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    Main s = new Main();
+	    int a=8;
+	    int b=7;
+	    System.out.print(s.add(a,b));
+	}
+	int add(int x,int y){
+	    int g=x+y;
+	    return g;
+	}
+	}
+
+
